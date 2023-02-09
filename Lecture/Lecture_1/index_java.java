9af164e9495ad149c75a0a11bbe9b8c63cd9eeba
medvedit <113551136+medvedit@users.noreply.github.com>
2023-02-09 14:59:37 +0300

    обрашение к индексу

---
+++ Lecture/Lecture_1/index_java.java
@@ -0,0 +1,13 @@
+/*
+ * Обращение к индексу на примере строки.
+ */
+package Java_GB.Lecture.Lecture_1;
+
+public class index_java {
+    public static void main(String[] args) {
+        String i = "gdhfjs";
+        i.charAt(1); // обращение к индексу строки.
+        System.out.println(i.charAt(1)); // d -> вывод значения первого индекса строки.
+    }
+
+}
