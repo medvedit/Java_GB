e3fd01d3f3db882c3f2ef53d3f5d37fdb10960bc
medvedit <113551136+medvedit@users.noreply.github.com>
2023-02-09 14:58:27 +0300

    определение типа данных

---
+++ Lecture/Lecture_1/get_Type.java
@@ -0,0 +1,30 @@
+/*
+ * Не явная типизация:
+ * Что бы определить к какому типу данных приведено значение перемененной var(не явная типизация)
+ * используется метод getType.
+ */
+package Java_GB.Lecture.Lecture_1;
+
+/**
+ * get_Type
+ */
+public class get_Type {
+    public static void main(String[] args) {
+        var a = 123;
+        System.out.println(a); // 123
+        var d = 123.456;
+        System.out.println(d); // 123.456
+        System.out.println(getType(a)); // Integer
+        System.out.println(getType(d)); // Double
+        d = 1022;
+        System.out.println(d); // 1022.0
+        // d = "mistake";
+        // error: incompatible types:
+        // String cannot be converted to double
+    }
+
+    static String getType(Object o) {
+        return o.getClass().getSimpleName();
+    }
+
+}
