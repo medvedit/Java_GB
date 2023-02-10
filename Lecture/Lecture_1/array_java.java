ab88b4a1a1bf50b6ce5aa5761bc39b767dac11ef
medvedit <113551136+medvedit@users.noreply.github.com>
2023-02-10 12:17:29 +0300

    закончил конспект

---
+++ Lecture/Lecture_1/array_java.java
@@ -0,0 +1,16 @@
+/*
+ * создание одномерного массива заполненного нулями. Положили значение 13 на 3 индекс и вывели в консоль.
+ */
+
+package Java_GB.Lecture.Lecture_1;
+
+public class array_java {
+    public static void main(String[] args) {
+        int[] arr = new int[10];
+        arr[3] = 13;
+        System.out.println(arr[3]); // 13
+
+
+    }
+
+}
