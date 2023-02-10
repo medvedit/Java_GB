1ae68f44dc254d2a52acddb3b52968183a5cd00d
medvedit <113551136+medvedit@users.noreply.github.com>
2023-02-10 12:09:05 +0300

    start task_1

---
+++ Seminar/Seminar_1/homeWork/task_1.java
@@ -0,0 +1,13 @@
+/*
+ * Задача 1
+ * Вычислить n-ое треугольное число(сумма чисел от 1 до n), n! (произведение чисел от 1 до n)
+ */
+
+package Java_GB.Seminar.Seminar_1.homeWork;
+
+public class task_1 {
+    public static void main(String[] args) {
+
+    }
+
+}
