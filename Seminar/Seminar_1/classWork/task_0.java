4d16cba084cd0574815b83c155140d1505e7cb6f
medvedit <113551136+medvedit@users.noreply.github.com>
2023-02-10 11:46:27 +0300

    end task_0

---
+++ Seminar/Seminar_1/classWork/task_0.java
@@ -0,0 +1,11 @@
+package Java_GB.Seminar.Seminar_1.classWork;
+
+import java.time.LocalDateTime;
+
+public class task_0 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println(LocalDateTime.now());
+
+    }
+}
