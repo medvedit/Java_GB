5c59ef0d79e37969b0feda1ab6fc52262657655a
medvedit <113551136+medvedit@users.noreply.github.com>
2023-02-10 11:53:56 +0300

    end task_1

---
+++ Seminar/Seminar_1/classWork/task_1.java
@@ -0,0 +1,45 @@
+/*
+ * Задание №1
+Написать программу, которая запросит пользователя ввести <Имя> в консоли.
+Получить введенную строку и выведет в консоль сообщение “Привет, <Имя>!”
+
+ * Задание №1*
+В консоли запросить имя пользователя. В зависимости от текущего времени, вывести приветствие вида
+"Доброе утро, <Имя>!", если время от 05:00 до 11:59
+"Добрый день, <Имя>!", если время от 12:00 до 17:59;
+"Добрый вечер, <Имя>!", если время от 18:00 до 22:59;
+"Доброй ночи, <Имя>!", если время от 23:00 до 4:59
+
+ */
+
+package Java_GB.Seminar.Seminar_1.classWork;
+
+import java.time.LocalTime;
+import java.util.Scanner;
+
+public class task_1 {
+        // задание 1
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите имя: ");
+        String name = scanner.next();
+        System.out.println("Привет, "+ name);
+
+        // задание 1*
+        int hour = LocalTime.now().getHour();
+
+        if (hour >= 5 && hour < 12) {
+            System.out.println("Доброе утро,");
+        } else if (hour>=12 && hour<18) {
+            System.out.print("Добрый день, ");
+        } else if (hour>=18 && hour<23) {
+            System.out.print("Добрый вечер, ");
+        } else {
+            System.out.print("Доброй ночи, ");
+        }
+        System.out.println(name);
+
+
+    }
+
+}
