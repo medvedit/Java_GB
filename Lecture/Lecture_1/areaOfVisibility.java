9a5e4644ec5a2a13b5928b42aea50dc10824005f
medvedit <113551136+medvedit@users.noreply.github.com>
2023-02-11 16:47:07 +0300

    end области видимости переменных

---
+++ Lecture/Lecture_1/areaOfVisibility.java
@@ -0,0 +1,35 @@
+/*
+ *  Область видимости переменных:
+ */
+
+package Java_GB.Lecture.Lecture_1;
+
+public class areaOfVisibility {
+    public static void main(String[] args) {
+
+        // У переменных существует понятие «область видимости».
+        // Если переменную объявили внутри некоторого блока фигурных скобок { },
+        // то снаружи этого блока переменная будет недоступна.
+
+        {
+            int i = 123;
+            System.out.println(i); //123
+        }
+        // System.out.println(i); // error: cannot find symbol
+
+
+        // Но так работать будет норм:
+        int f = 123;
+        {
+            int u = 111;
+            int s = f + u;
+            System.out.println(s); //234
+        }
+        int t = 10;
+        int y = f + t;
+        System.out.println(y); // 133
+
+    }
+
+}
+
