604a3a8593f9b6d7d29e95343b2b8a43375a887c
medvedit <113551136+medvedit@users.noreply.github.com>
2023-02-12 22:11:20 +0300

    end

--- Lecture/Lecture_1/functionsMethods.java
+++ Lecture/Lecture_1/functionsMethods.java
@@ -20,7 +20,7 @@ public class functionsMethods {
         if (n == 1)
             return 1;
         if (n == 0)
-            return 0;
+            return 1;
         return n * factor(n - 1);
     }
 
