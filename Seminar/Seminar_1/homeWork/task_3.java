ed63dac941c8be8b05d7dbe03af595d6358a9b6e
medvedit <113551136+medvedit@users.noreply.github.com>
2023-02-13 11:26:54 +0300

    вынес функционал ввода цифр и символов в отдельный метод

--- Seminar/Seminar_1/homeWork/task_3.java
+++ Seminar/Seminar_1/homeWork/task_3.java
@@ -7,29 +7,46 @@ package Java_GB.Seminar.Seminar_1.homeWork;
 import java.util.Scanner;
 
 public class task_3 {
+    static Scanner scanner = new Scanner(System.in);
     public static void main(String[] args) {
-        try (Scanner scanner = new Scanner(System.in)) {
-            System.out.printf("Введите первое число: ");
-            int num1 = scanner.nextInt();
-            System.out.printf("Введите второе число: ");
-            int num2 = scanner.nextInt();
-            System.out.print("Введите знак операции: ");
-            char a = (char) System.in.read();
-            scanner.close();
-            if (a == '+') {
-                System.out.printf("%d + %d = %d\n", num1, num2, sum(num1, num2));
-            } else if (a == '-') {
-                System.out.printf("%d - %d = %d\n", num1, num2, difference(num1, num2));
-            } else if (a == '*') {
-                System.out.printf("%d * %d = %d\n", num1, num2, multiply(num1, num2));
-            } else if (a == '/') {
-                division(num1, num2);
-            }
 
-        } catch (Exception ex) {
-            System.out.println("Вы ввели не целое число или строку, будьте внимательнее.");
+        int num1 = getInt();
+        int num2 = getInt();
+        char a = getOperation();
+        if (a == '+') {
+            System.out.printf("%d + %d = %d\n", num1, num2, sum(num1, num2));
+        } else if (a == '-') {
+            System.out.printf("%d - %d = %d\n", num1, num2, difference(num1, num2));
+        } else if (a == '*') {
+            System.out.printf("%d * %d = %d\n", num1, num2, multiply(num1, num2));
+        } else if (a == '/') {
+            division(num1, num2);
         }
 
+    }
+    public static int getInt() {
+        System.out.println("Введите число:");
+        int num;
+        if (scanner.hasNextInt()) {
+            num = scanner.nextInt();
+        } else {
+            System.out.println("Вы допустили ошибку при вводе числа. Попробуйте еще раз.");
+            scanner.next();// рекурсия
+            num = getInt();
+        }
+        return num;
+    }
+    public static char getOperation() {
+        System.out.println("Введите операцию:");
+        char operation;
+        if (scanner.hasNext()) {
+            operation = scanner.next().charAt(0);
+        } else {
+            System.out.println("Вы допустили ошибку при вводе операции. Попробуйте еще раз.");
+            scanner.next();// рекурсия
+            operation = getOperation();
+        }
+        return operation;
     }
 
     static int sum(int a, int b) {
