55c02f7b0912061694072806d69e19a5043ec3ef
medvedit <113551136+medvedit@users.noreply.github.com>
2023-02-18 00:43:18 +0300

    отредактировал вывод, добавил коммент к коду

--- Seminar/Seminar_3/homeWork/task_1.java
+++ Seminar/Seminar_3/homeWork/task_1.java
@@ -14,35 +14,36 @@
 
      public static void main(String[] args) {
 
-         List<Integer> randomList = myNumbers();
-         maxMinAverage(randomList);
+         List<Integer> randomList = myNumbers(); // создали заполненный целыми числами список.
+         maxMinAverage(randomList); // нашли значение Max, значение Min и среднее арифметическое.
 
      }
-     public static ArrayList<Integer> myNumbers(){
-         ArrayList<Integer> numberList = new ArrayList<>();
+     public static ArrayList<Integer> myNumbers(){ // метод создания списка Random.
+         ArrayList<Integer> numberList = new ArrayList<>(); // создали новы список для заполнения.
          Random random = new Random();
 
-         for (int i = 0; i < 11; i++) {
-             numberList.add(random.nextInt(0, 100));
-         }System.out.println(numberList);
-         return numberList;
+         for (int i = 0; i < 11; i++) { // прошли в цикле, создавая список из 11 элементов.
+             numberList.add(random.nextInt(0, 100)); // range от 0 до 99
+         } System.out.println(numberList); // вывод в консоль для визуальной проверки ответов второй функции.
+         return numberList; // возврат в main заполненного списка.
 
      }
-     public static void maxMinAverage(List<Integer> numList){
+     public static void maxMinAverage(List<Integer> numList){ // метод поиска Max, Min и Average
 
-         int numMax = numList.get(0);
-         int numMin = numList.get(0);
-         double sum = numList.get(0);
+         int numMax = numList.get(0); // создал три переменных
+         int numMin = numList.get(0);             // каждую прировнял
+         double sum = numList.get(0);                       // к элементу нулевого индекса.
 
-         for (int i = 1; i < numList.size(); i++) {
-             sum = sum + numList.get(i);
-             if(numList.get(i) > numMax){
-                 numMax = numList.get(i);
-             }if(numList.get(i) < numMin){
-                 numMin = numList.get(i);
+         for (int i = 1; i < numList.size(); i++) { // проход с элемента с индексом 1
+             sum = sum + numList.get(i); // общая сумма всех значений
+             if(numList.get(i) > numMax){ // поиск Max
+                 numMax = numList.get(i); // сохранение Max
+             }if(numList.get(i) < numMin){ // поиск Min
+                 numMin = numList.get(i); // сохранение Min
              }
 
-         }System.out.printf("Max %d, Min %d, Average %.2f\n",numMax, numMin, sum/numList.size());
+         } System.out.printf("Max %d, \nMin %d, \nAverage %.2f\n"
+                                        ,numMax, numMin, sum/numList.size()); // вывод всех значений
 
      }
 
