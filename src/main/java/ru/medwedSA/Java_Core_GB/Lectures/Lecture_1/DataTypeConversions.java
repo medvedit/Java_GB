cac6ffad800c927d4b7cfea94a51778198c848db
medvedit <113551136+medvedit@users.noreply.github.com>
2023-06-30 12:34:10 +0300

    Явные и не явные преобразования

--- src/main/java/ru/medwedSA/Java_Core_GB/Lectures/Lecture_1/DataTypeConversions.java
+++ src/main/java/ru/medwedSA/Java_Core_GB/Lectures/Lecture_1/DataTypeConversions.java
@@ -18,16 +18,19 @@ public class DataTypeConversions {
         System.out.println(i); // 123
         System.out.println(d); // 123.0
 
-        i = (int)d;
+        i = (int)d; // явное преобразование double в int.
         d = 3.1415;
-        System.out.println(d); // 3.1415
         System.out.println(i); // 123
+        System.out.println(d); // 3.1415
 
-        d = 3.9415; i = (int)d;
+        d = 3.9415;
+        i = (int)d; // явное преобразование double в int.
         System.out.println(d); // 3.9415
         System.out.println(i); // 3
 
-        d = Byte.parseByte("123");
+        d = Byte.parseByte("123"); // Явное преобразование. Проверка строки - состоит из цифр, если да, то приводим
+                                      // их в тип данных byte. Если цифры поменяются в byte, то явно приводим к
+                                      // типу данных double.
         System.out.println(d); // 123.0
         // d = Byte.parseByte("1234"); // в byte 4 символа не войдет.
         // System.out.println(d); // NumberFormatException: Value out of range
@@ -36,6 +39,7 @@ public class DataTypeConversions {
         // Просто так массив int положить в массив double не получится, нужны костыли.
         // int[] a = new int[10];
         // double[] c = a;   // ИЗУЧАЕМ!!! ИНВАРИАНТЫ -> ковариантность и контравариантность
+        // Посмотрел тут -> https://www.youtube.com/watch?v=2yeFSrcTQh8
 
     }
 }
