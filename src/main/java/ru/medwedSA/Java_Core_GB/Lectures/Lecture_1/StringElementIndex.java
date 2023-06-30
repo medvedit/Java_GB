b0b8ec6e41acc82d2e05956970d6fa10d2e766e4
medvedit <113551136+medvedit@users.noreply.github.com>
2023-06-30 10:32:37 +0300

    Очень немного о работе со строками

--- src/main/java/ru/medwedSA/Java_Core_GB/Lectures/Lecture_1/StringElementIndex.java
+++ src/main/java/ru/medwedSA/Java_Core_GB/Lectures/Lecture_1/StringElementIndex.java
@@ -1,13 +1,21 @@
+package ru.medwedSA.Java_Core_GB.Lectures.Lecture_1;
+
 /*
+ * String - ЭТО ССЫЛОЧНЫЙ ТИА ДАННЫХ!
  * Обращение к индексу на примере строки.
  */
-package ru.medwedSA.Java_Core_GB.Lectures.Lecture_1;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Arrays;
 
 public class StringElementIndex {
     public static void main(String[] args) {
-        String i = "gdhfjs";
-        i.charAt(1); // обращение к индексу строки.
-        System.out.println(i.charAt(1)); // d -> вывод значения первого индекса строки.
+        String str = "denote";
+        str.charAt(1); // обращение к индексу строки.
+        System.out.println(str.charAt(1)); // e -> вывод значения первого индекса строки.
+        System.out.println(str.length()); // получение длины строки.
+        System.out.println(Arrays.toString(str.getBytes(StandardCharsets.UTF_8))); // вывод в консоль
+                                                                             // массива хеш кодов символов строки.
     }
 
 }
