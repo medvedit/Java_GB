dd4fdd5d60ae097a78a0a4c3027311e4fb54d611
medvedit <113551136+medvedit@users.noreply.github.com>
2023-07-03 15:16:02 +0300

    Массив. Вывести максимальное количество подряд идущих 1.

--- src/main/java/ru/medwedSA/Java_Core_GB/Seminars/Seminar_1/ClassWork/task_2.java
+++ src/main/java/ru/medwedSA/Java_Core_GB/Seminars/Seminar_1/ClassWork/task_2.java
@@ -7,14 +7,14 @@ package ru.medwedSA.Java_Core_GB.Seminars.Seminar_1.ClassWork;
 
  */
 
-public class task_2 {
+public class   task_2 {
     public static void main(String[] args) {
         int[] arr = new int[] { 1, 1, 0, 1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 0, 0, 0 };
 
         int count = 0;
         int maxCount = 0;
-        for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == 1) {
+        for (int i : arr) {
+            if (i == 1) {
                 count++;
                 if (count > maxCount) {
                     maxCount = count;
