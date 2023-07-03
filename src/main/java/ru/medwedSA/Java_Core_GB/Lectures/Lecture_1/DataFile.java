aba374a0b285e83cbe070a16db1aef469d9f3f9b
medvedit <113551136+medvedit@users.noreply.github.com>
2023-07-03 11:03:52 +0300

    Создание классов для создания файла, записи в файл и для чтения из фала.

--- src/main/java/ru/medwedSA/Java_Core_GB/Lectures/Lecture_1/DataFile.java
+++ src/main/java/ru/medwedSA/Java_Core_GB/Lectures/Lecture_1/DataFile.java
@@ -6,38 +6,41 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class DataFile {
+    private static final String addressFileText = "/Users/Medwed_SA/Desktop/Education/Java/project_IntelliJ_IDEA/" +
+            "Java_GB/src/main/java/ru/medwedSA/Java_Core_GB/" +
+            "Lectures/Lecture_1/text.txt"; // адрес обращения к файлу text.txt
+    private static final String addressFileNotes = "/Users/Medwed_SA/Desktop/Education/Java/project_IntelliJ_IDEA/" +
+            "Java_GB/src/main/java/ru/medwedSA/Java_Core_GB/" +
+            "Lectures/Lecture_1/notes.txt"; // адрес обращения к файлу notes.txt
+
     public static void main(String[] args) {
 
-        dataIn();
-        dataOutStr();
-        readWriteData();
+        createFileNameTextWriteToFile();
+        readDataFileNameText();
+        writeDataFileNameNotes();
+        readDataFileNameNotes();
 
     }
 
-    // Создание файла. Запись и до запись в файл.
-    static void dataIn() {
-        try (FileWriter fw = new FileWriter("/Users/" +
-                "Medwed_SA/Desktop/Education/Java/project_IntelliJ_IDEA/" +
-                "Java_GB/src/main/java/ru/medwedSA/Java_Core_GB/" +
-                "Lectures/Lecture_1/text.txt", false)) { // false -> Создает файл и перезаписывает.
-                                                                 // true -> Создает файл и до записывает в файл.
+    static void createFileNameTextWriteToFile() { // Создание файла. Запись и до запись в файл (true или false).
+        // try-with-resources с авто закрытием.
+        try (FileWriter fw = new FileWriter(addressFileText, false)) {
+                                                                        // false -> Создает файл и перезаписывает.
+                                                                        // true -> Создает файл и дописать в файл.
             fw.write("lina 1\n");
             fw.append('2');
             fw.append('\n');
             fw.write("lina 3\n");
-            fw.flush();
-        } catch (IOException ex) {
-            System.out.println(ex.getMessage());
+            fw.flush(); // Этот метод используется для очистки модуля записи, что означает, что этот метод удалит все
+                        // данные, присутствующие внутри модуля записи.
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
         }
-
     }
 
-    // Чтение из файла. Вариант посимвольно.
-    static void dataOutStr() {
-        try (FileReader fr = new FileReader("/Users/" +
-                "Medwed_SA/Desktop/Education/Java/project_IntelliJ_IDEA/" +
-                "Java_GB/src/main/java/ru/medwedSA/Java_Core_GB/" +
-                "Lectures/Lecture_1/text.txt")) {
+    static void readDataFileNameText() { // Чтение из файла. Вариант посимвольно.
+        try (FileReader fr = new FileReader(addressFileText)) { // try-with-resources с авто закрытием.
             int c;
             while ((c = fr.read()) != -1) {
                 char ch = (char) c;
@@ -45,53 +48,46 @@ public class DataFile {
                     System.out.println(ch);
                 } else {
                     System.out.println(ch);
-
                 }
             }
         } catch (IOException e) {
             System.out.println(e.getMessage());
-
         }
-
     }
 
-    static void readWriteData() {
-        // ЗАПИСЫВАНИЕ В ФАЙЛ
-        try (FileWriter writer = new FileWriter("/Users/" +
-                "Medwed_SA/Desktop/Education/Java/project_IntelliJ_IDEA/" +
-                "Java_GB/src/main/java/ru/medwedSA/Java_Core_GB/" +
-                "Lectures/Lecture_1/notes3.txt", false)){ // Создание файла в директории проекта
-          // в параметр append в строке выше добавить true, чтоб ДО-записывать, а не ПЕРЕ-записывать файл.
-          // Запись всей строки или переменной -
+
+
+    static void writeDataFileNameNotes() { // ЗАПИСЫВАНИЕ В ФАЙЛ
+        try (FileWriter writer = new FileWriter(addressFileNotes, false)){ // Создание файла по
+            // указанному адресу. false -> перезаписать, а true -> дописать в файл.
+            // try-with-resources с авто закрытием.
             String text = "Java может, " +
                     "Java может всё что угодно!\nМожет это. Может это\n..." +
-                    "Но не может банально вывести на консоль тип переменной...";
+                    "Но не может банально вывести на консоль тип переменной..."; // Текст для записи.
             writer.write(text);
-            // запись по символам -
-            writer.append('\n');
+            writer.append('\n'); // запись по символам
             writer.append("END\n");
-
-            writer.flush(); // Закрытие потока FileWriter, вероятно... Несмотря на то, что мы находимся в
-                            // try-with-resources с авто закрытием.
+            writer.flush(); // Этот метод используется для очистки модуля записи, что означает, что этот метод
+                            // удалит все данные, присутствующие внутри модуля записи.
         } catch (IOException e) {
             System.out.println(e.getMessage());
             e.printStackTrace();
         }
-        System.out.println("Создание и запись файла завершены");
+        System.out.println("Создание и запись файла завершены.");
+    }
 
 
 
-        // ЧТЕНИЕ ИЗ ФАЙЛА по строковое
-        System.out.println("\n\"Чтение из файла\"\n");
+    private static void readDataFileNameNotes() { // ЧТЕНИЕ ИЗ ФАЙЛА по строковое
+        System.out.println("\n\"Начало чтения из файла.\"\n");
 
-        try (FileReader fr = new FileReader("/Users/" +
-                "Medwed_SA/Desktop/Education/Java/project_IntelliJ_IDEA/" +
-                "Java_GB/src/main/java/ru/medwedSA/Java_Core_GB/" +
-                "Lectures/Lecture_1/notes3.txt"); BufferedReader reader = new BufferedReader(fr)){
+        String addressFile = "/Users/Medwed_SA/Desktop/Education/Java/project_IntelliJ_IDEA/Java_GB/src/main/java/" +
+                "ru/medwedSA/Java_Core_GB/Lectures/Lecture_1/notes.txt"; // адрес обращения к файлу.
+
+        try (FileReader fr = new FileReader(addressFile); BufferedReader reader = new BufferedReader(fr)){
         // создаем объект FileReader для файла notes3.txt. И создаем BufferedReader с
         // существующего FileReader для построчного считывания
             String line = reader.readLine(); // считаем сначала первую строку
-
             int count = 0; // счётчик считывания для наглядности, для себя делаю.
             while (line != null) {
                 System.out.printf("Считывание в цикле #%d: ", ++count); // Вывод счетчика.
@@ -101,7 +97,6 @@ public class DataFile {
         } catch (IOException e) {
             e.printStackTrace();
         }
-
     }
 
 }
