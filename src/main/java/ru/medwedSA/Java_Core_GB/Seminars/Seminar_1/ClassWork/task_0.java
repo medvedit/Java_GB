9493844aba4b509d4dae4e22df0421d78746b953
medvedit <113551136+medvedit@users.noreply.github.com>
2023-07-03 11:07:04 +0300

    Настроить проект, вывести в консоль “Hello world!”. Вывести в консоль системные дату и время.

---
+++ src/main/java/ru/medwedSA/Java_Core_GB/Seminars/Seminar_1/ClassWork/task_0.java
@@ -0,0 +1,17 @@
+package ru.medwedSA.Java_Core_GB.Seminars.Seminar_1.ClassWork;
+
+/*
+ *  Настроить проект, вывести в консоль “Hello world!”.
+ *  Вывести в консоль системные дату и время.
+ *
+ */
+
+import java.time.LocalDateTime;
+
+public class task_0 {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println(LocalDateTime.now());
+
+    }
+}
