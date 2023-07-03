9dcc2db82ac94c1e06c57911d75860bb79ab8de7
medvedit <113551136+medvedit@users.noreply.github.com>
2023-07-03 11:05:45 +0300

    Еще один вариант чтения из файла. В комментарии, в самом классе больше информации по использованию такого решения.

--- src/main/java/ru/medwedSA/Java_Core_GB/Lectures/Lecture_1/DataFile2.java
+++ src/main/java/ru/medwedSA/Java_Core_GB/Lectures/Lecture_1/DataFile2.java
@@ -2,19 +2,25 @@ package ru.medwedSA.Java_Core_GB.Lectures.Lecture_1;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
+import java.io.IOException;
 
 public class DataFile2 {
-    public static void main(String[] args) throws Exception{ // в этом примере возможная ошибка обработана throws Exception
+    /**
+     * Это решение схоже с решением из класса DataFile, в методе readDataFileNameNotes() Так же происходит считывание
+     * данных построчно из файла и вывод каждой строки в консоль. Но тут использован цикл while, по-этому принудительное
+     * закрытие соединения с файлом, кроме этого исключение не обработано, а просто проброшено.
+     * Правильнее, короче в написании использовать код с try-with-resources.
+     */
+    public static void main(String[] args) throws IOException { // в этом примере возможная ошибка обработана throws IOException
                                                             // а не try catch как в файле dataFile.java
-        BufferedReader br = new BufferedReader(new FileReader("/Users/" +
-                "Medwed_SA/Desktop/Education/Java/project_IntelliJ_IDEA/" +
-                "Java_GB/src/main/java/ru/medwedSA/Java_Core_GB/" +
-                "Lectures/Lecture_1/notes3.txt"));
+        BufferedReader br = new BufferedReader(new FileReader("/Users/Medwed_SA/Desktop/Education/" +
+                "Java/project_IntelliJ_IDEA/Java_GB/src/main/java/ru/medwedSA/" +
+                "Java_Core_GB/Lectures/Lecture_1/notes.txt"));
         String str;
-        while ((str = br.readLine()) != null){
+        while ((str = br.readLine()) != null){ // пока br.readLine() не пуст, пока есть что читать...
             System.out.printf("== %s ==\n", str);
         }
-        br.close();
+        br.close(); // закрытие соединения с файлом.
 
     }
 
