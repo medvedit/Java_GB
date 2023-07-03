8174c490b7f32fa30465d5ec38d1e181156da18e
medvedit <113551136+medvedit@users.noreply.github.com>
2023-07-03 15:16:35 +0300

    Массив. Сортировка относительно заданного числа.

--- src/main/java/ru/medwedSA/Java_Core_GB/Seminars/Seminar_1/ClassWork/task_3.java
+++ src/main/java/ru/medwedSA/Java_Core_GB/Seminars/Seminar_1/ClassWork/task_3.java
@@ -5,17 +5,16 @@ package ru.medwedSA.Java_Core_GB.Seminars.Seminar_1.ClassWork;
 Дан массив num = [3, 2, 2, 3, 3, 2, 2, 2, 3, 2, 2] и число val = 3.
 Если в массиве есть числа, равные заданному, нужно перенести эти элементы в конец массива.
 Таким образом, первые несколько (или все) элементов массива должны быть отличны от заданного, а остальные - равны ему.
-
  */
 
 import java.util.Arrays;
 
 public class task_3 {
     public static void main(String[] args) {
-        int[] arr = {3, 2, 2, 3, 3, 2, 2, 2, 3, 2, 2};
+        int[] arr = {2, 3, 2, 2, 3, 3, 2, 2, 2, 3, 2, 2};
         int num = 3;
         int j = 0;
-        int temp = 0;
+        int temp;
         for (int i = 0; i < arr.length; i++) {
             if (arr[i] != num) {
                 temp = arr[j];
