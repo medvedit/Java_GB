50f40272e9e2b08d9d38fcdca51ba77526b2a8bc
medvedit <113551136+medvedit@users.noreply.github.com>
2023-07-04 12:39:16 +0300

    Работа с файлами.

---
+++ src/main/java/ru/medwedSA/Java_Core_GB/Lectures/Lecture_2/loggingFile.java
@@ -0,0 +1,70 @@
+package ru.medwedSA.Java_Core_GB.Lectures.Lecture_2;
+
+/*
+ *  Логирование
+    Логи содержат системную информацию работы программного или аппаратного комплекса.
+    В них записываются действия разного приоритета от пользователя, или программного продукта.
+    Процесс ведения логов называют “логированием” (журналированием).
+
+    Логирование. Использование
+    Feb 14 1994 05:08:33 WATCHDOG: [FAUPGRADE][auto_firmware_check:(7285)]no need to upgrade firmware
+    Feb 14 1994 07:03:41 ntp: start NTP update
+    Feb 14 1994 10:14:02 syslog: wlceventd_proc_event(527): eth1: Auth BC:DD:C2:88:E2:3F, status: Successful (0), rssi:0
+    Feb 14 1994 10:14:02 syslog: wlceventd_proc_event(556): eth1: Assoc BC:DD:C2:88:E2:3F, status: Successful (0), rssi:0
+    Feb 14 1994 10:20:20 syslog: wlceventd_proc_event(527): eth1: Auth D4:A6:51:07:54:BB, status: Successful (0), rssi:0
+    Feb 14 1994 10:20:20 syslog: wlceventd_proc_event(556): eth1: Assoc D4:A6:51:07:54:BB, status: Successful (0), rssi:0
+    Feb 14 1994 10:20:20 syslog: wlceventd_proc_event(527): eth1: Auth D4:A6:51:01:F4:E9, status: Successful (0), rssi:0
+    Feb 14 1994 10:20:20 syslog: wlceventd_proc_event(556): eth1: Assoc D4:A6:51:01:F4:E9, status: Successful (0), rssi:0
+    Feb 14 1994 11:39:08 syslog: wlceventd_proc_event(491): eth1: Deauth_ind 48:3F:DA:91:19:E0, status: 0, reason: Disassociated due to inactivity (4), rssi:0 Feb 14 1994 13:03:06 syslog: wlceventd_proc_event(491): eth2: Deauth_ind 18:3E:EF:E9:DF:15, status: 0, reason: Disassociated due to inactivity (4), rssi:0 Feb 14 1994 13:03:08 syslog: wlceventd_proc_event(491): eth1: Deauth_ind BC:DD:C2:88:E2:3F, status: 0, reason: Disassociated due to inactivity (4), rssi:0 Feb 14 1994 13:03:19 syslog: wlceventd_proc_event(491): eth2: Deauth_ind C2:D4:93:FB:9F:FB, status: 0, reason: Disassociated due to inactivity (4), rssi:0 Feb 14 1994 13:03:31 syslog: wlceventd_proc_event(491): eth2: Deauth_ind FE:1F:80:6E:4B:09, status: 0, reason: Disassociated due to inactivity (4), rssi:0 Feb 14 1994 13:06:34 syslog: wlceventd_proc_event(491): eth1: Deauth_ind D4:A6:51:01:F4:E9, status: 0, reason: Disassociated due to inactivity (4), rssi:0 Feb 14 1994 13:06:34 syslog: wlceventd_proc_event(491): eth1: Deauth_ind D4:A6:51:07:54:BB, status: 0, reason: Disassociated due to inactivity (4), rssi:0 Feb 14 1994 18:07:09 syslog: wlceventd_proc_event(527): eth1: Auth 72:34:C1:43:EC:16, status: Successful (0), rssi:0
+    Feb 14 1994 18:07:09 syslog: wlceventd_proc_event(527): eth1: Auth 72:34:C1:43:EC:16, status: Successful (0), rssi:0
+    Feb 14 1994 18:07:09 syslog: wlceventd_proc_event(556): eth1: Assoc 72:34:C1:43:EC:16, status: Successful (0), rssi:0
+    Feb 14 1994 18:07:23 syslog: wlceventd_proc_event(527): eth1: Auth FA:7F:A4:CD:2F:4B, status: Successful (0), rssi:0
+    Feb 14 1994 18:07:23 syslog: wlceventd_proc_event(556): eth1: Assoc FA:7F:A4:CD:2F:4B, status: Successful (0), rssi:0
+    Feb 14 1994 18:08:10 syslog: wlceventd_proc_event(527): eth1: Auth 84:CC:A8:86:02:33, status: Successful (0), rssi:0
+    Feb 14 1994 18:08:10 syslog: wlceventd_proc_event(556): eth1: Assoc 84:CC:A8:86:02:33, status: Successful (0), rssi:0
+    Feb 14 1994 18:08:15 syslog: wlceventd_proc_event(527): eth1: Auth BC:DD:C2:88:E2:3F, status: Successful (0), rssi:0
+    Feb 14 1994 18:08:15 syslog: wlceventd_proc_event(556): eth1: Assoc BC:DD:C2:88:E2:3F, status: Successful (0), rssi:0
+    Feb 14 1994 18:09:14 syslog: wlceventd_proc_event(527): eth2: Auth 18:3E:EF:E9:DF:15, status: Successful (0), rssi:0
+    Feb 14 1994 18:09:14 syslog: wlceventd_proc_event(556): eth2: Assoc 18:3E:EF:E9:DF:15, status: Successful (0), rssi:0
+
+     Логирование. Использование. Основы
+
+    Использование
+    Logger logger = Logger.getLogger(<указать тип, наименование класса>)
+
+    Уровни важности
+    INFO, DEBUG, ERROR, WARNING и др.
+
+    Вывод
+    ConsoleHandler info = new ConsoleHandler(); log.addHandler(info);
+
+    Формат вывода: структурированный, абы как*
+    XMLFormatter, SimpleFormatter
+ */
+
+import java.io.IOException;
+import java.util.logging.*;
+
+
+public class loggingFile {
+    public static void main(String[] args) throws IOException {
+
+        Logger logger = Logger.getLogger(loggingFile.class.getName());
+        ConsoleHandler ch = new ConsoleHandler();
+         FileHandler fh = new FileHandler("/Users/Medwed_SA/Desktop/Education/" +
+                 "Java/project_IntelliJ_IDEA/Java_GB/src/main/java/ru/medwedSA/" +
+                 "Java_Core_GB/Lectures/Lecture_2/log.xml");
+//        logger.addHandler(ch);
+         logger.addHandler(fh);
+
+        SimpleFormatter sFormat = new SimpleFormatter();
+         XMLFormatter xml = new XMLFormatter();
+//        ch.setFormatter(sFormat);
+         fh.setFormatter(xml);
+
+//        logger.setLevel(Level.INFO);
+        logger.log(Level.WARNING, "Тестовое логирование 1");
+        logger.info("Тестовое логирование 2");
+
+    }
+}
---
+++ src/main/java/ru/medwedSA/Java_Core_GB/Lectures/Lecture_2/systemFile.java
@@ -0,0 +1,156 @@
+package ru.medwedSA.Java_Core_GB.Lectures.Lecture_2;
+
+/*
+    Для работы нужно:
+    File <имя> = new File(<полный путь к файлу>);
+    File f1 = new File("file.txt");
+    File f2 = new File("/Users/sk/vscode/java_projects/file.txt");
+
+    Работа с файловой системой:
+
+    isHidden():          возвращает истину, если каталог или файл является скрытым
+    length():            возвращает размер файла в байтах
+    lastModified():      возвращает время последнего изменения файла или каталога
+    list():              возвращает массив файлов и подкаталогов, которые находятся в каталоге
+    listFiles():         возвращает массив файлов и подкаталогов, которые находятся в определенном каталоге
+    mkdir():             создает новый каталог
+    renameTo(File dest): переименовывает файл или каталог
+    length():            возвращает размер файла в байтах
+    lastModified():      возвращает время последнего изменения файла или каталога
+    list():              возвращает массив файлов и подкаталогов, которые находятся в каталоге
+    listFiles():         возвращает массив файлов и подкаталогов, которые находятся в определенном каталоге
+    mkdir():             создает новый каталог
+    renameTo(File dest): переименовывает файл или каталог
+ */
+
+import java.io.File;
+import java.io.FileWriter;
+
+public class systemFile {
+
+    private static final String USER_DIR = "user.dir";
+    private static final String ADDRESS_FILE_TXT = "/files/file.txt";
+    public static void main(String[] args) {
+
+        fileWork();
+        fileWork2();
+        fileWork3();
+        fileWork4();
+        fileWork5(); // метод создающий в рабочем проекте папку files
+    }
+
+    static void fileWork() {
+        String pathProject = System.getProperty(USER_DIR);
+        String pathFile = pathProject.concat(ADDRESS_FILE_TXT);
+
+        File f1 = new File(ADDRESS_FILE_TXT);
+        File f2 = new File("/Users/Medwed_SA/Desktop/Education/Java/project_IntelliJ_IDEA/Java_GB/src/main/java/ru/medwedSA/Java_Core_GB/Lectures/Lecture_2/file.txt");
+        System.out.println(f1.getAbsolutePath());
+        System.out.println(f2.getAbsolutePath());
+
+        File f3 = new File(pathFile);
+        System.out.println(f3.getAbsolutePath());
+        System.out.println(pathFile);
+    }
+
+    /*
+     * Работа с файловой системой. Файлы. Ошибки:
+     *
+     * try {
+     * Код, в котором может появиться ошибка
+     * } catch (Exception e) {
+     * Обработка, если ошибка случилась
+     * }
+     * finally {
+     * Код, который выполнится в любом случае
+     * }
+     */
+    static void fileWork2() { // НО если есть возможность избавиться от написания блока rey catch
+                              // то, необходимо это делать.
+        try {
+            String pathProject = System.getProperty(USER_DIR);
+            String pathFile = pathProject.concat(ADDRESS_FILE_TXT);
+            File f3 = new File(pathFile);
+            System.out.println("try");
+            System.out.println(f3);
+        } catch (Exception e) {
+            System.out.println("catch");
+        } finally {
+            System.out.println("finally");
+        }
+    }
+
+    static void fileWork3() {
+        try {
+            String pathProject = System.getProperty(USER_DIR);
+            String pathFile = pathProject.concat(ADDRESS_FILE_TXT);
+            File file = new File(pathFile);
+            if (file.createNewFile()) {
+                System.out.println("file.created");
+            } else {
+                System.out.println("file.existed");
+            }
+        } catch (Exception e) {
+            System.out.println("catch");
+        } finally {
+            System.out.println("finally");
+        }
+    }
+
+    static void fileWork4() {
+        // Работа с файловой системой. Файлы. Ошибки
+        String line = "empty";
+        try {
+            String pathProject = System.getProperty(USER_DIR);
+            String pathFile = pathProject.concat(ADDRESS_FILE_TXT);
+            File file = new File(pathFile);
+
+            if (file.createNewFile()) {
+                System.out.println("file.created");
+            }
+            else {
+                System.out.println("file.existed");
+                FileWriter fileWriter = new FileWriter(file, true);
+                fileWriter.write("new Line_1");
+
+                //#region lineSeparator
+                // A string containing "\r\n" for non-Unix
+                // platforms, or a string containing
+                // "\n" for Unix platforms.
+                fileWriter.append(System.lineSeparator());
+                //#endregion
+
+                fileWriter.write("new Line_2");
+                fileWriter.append('\n');
+                fileWriter.append("new Line_3");
+                fileWriter.append('\n');
+                fileWriter.flush();
+                fileWriter.close();
+                // BufferedReader bufReader = new BufferedReader(new FileReader(file));
+                // line = bufReader.readLine();
+                // bufReader.close();
+            }
+        } catch (Exception e) {
+            //e.printStackTrace();
+        } finally {
+            System.out.println(line);
+        }
+    }
+
+    static void fileWork5() {
+        // Работа с файловой системой. Каталоги
+        String pathProject = System.getProperty(USER_DIR);
+        String pathDir = pathProject.concat("/files");
+        File dir = new File(pathDir);
+        System.out.println(dir.getAbsolutePath());
+        if (dir.mkdir()) {
+            System.out.println("+");
+        } else {
+            System.out.println("-");
+        }
+        for (String fName : dir.list()) {
+            System.out.println(fName);
+        }
+    }
+
+}
