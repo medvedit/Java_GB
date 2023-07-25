1759b9abc7b54b4862bb702cd2cbcb88661b2a0f
medvedit <113551136+medvedit@users.noreply.github.com>
2023-07-25 11:10:20 +0300

    не нужен

--- src/main/java/ru/medwedSA/Java_Core_GB/Seminars/Seminar_3/ClassWork/Main.java
+++
@@ -1,8 +0,0 @@
-package ru.medwedSA.Java_Core_GB.Seminars.Seminar_3.ClassWork;
-
-public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-
-}
